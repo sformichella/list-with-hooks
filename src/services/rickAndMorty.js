55ce43b9d56538e1c6f92790f11890799d58bdf9
sformichella <sformichella@gmail.com>
2021-02-08 15:59:09 -0800

    rickAndMorty service

---
+++ src/services/rickAndMorty.js
@@ -0,0 +1,18 @@
+const API_URL = 'https://rickandmortyapi.com/api/';
+
+const getCharacters = () => {
+  return fetch(API_URL + 'character')
+    .then(res => res.json())
+    .then(json => json.results);
+};
+
+const getCharacter = id => {
+  return fetch(API_URL + `character?id=${id}`)
+    .then(res => res.json())
+    .then(json => json.results);
+};
+
+export {
+  getCharacter,
+  getCharacters
+};
