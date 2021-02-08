9bb91ec63d3822bdd72af11b5f820e803ddf6a06
sformichella <sformichella@gmail.com>
2021-02-08 15:31:58 -0800

    add routes

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
-export default function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => {
+  return (
+    <>
+      <main>
+        <Router>
+          <Switch>
+            <Route
+              path = "/"
+              exact
+              render = {routerProps => <List {...routerProps}/>}
+            />
+            <Route
+              path = "/:id"
+              exact
+              render = {routerProps => <Details {...routerProps}/>}
+            />
+          </Switch>
+        </Router>
+      </main>
+    </>
+  );
+};
+
+export default App;
