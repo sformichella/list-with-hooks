923f880dff49669113896f460a7b14d5ccf101e1
sformichella <sformichella@gmail.com>
2021-02-08 16:18:39 -0800

    add propTypes to Details

--- src/components/containers/Details/Details.jsx
+++ src/components/containers/Details/Details.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { getCharacter } from '../../../services/rickAndMorty';
 import CharacterDetails from '../../presentation/CharacterDetails/CharacterDetails';
 
@@ -18,3 +19,10 @@ export default function Details({ match }) {
   );
 }
 
+Details.propTypes = {
+  match: PropTypes.objectOf(PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.shape({
+      id: PropTypes.string.isRequired
+    }))
+  }))
+};
