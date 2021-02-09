eedb20a3d363c1129eac7bb8ec45ba008d0864d2
sformichella <sformichella@gmail.com>
2021-02-08 16:08:31 -0800

    Detials

--- src/components/containers/Details/Details.jsx
+++ src/components/containers/Details/Details.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getCharacter } from '../../../services/rickAndMorty';
+import CharacterDetails from '../../presentation/CharacterDetails/CharacterDetails';
 
-export default function Details() {
+export default function Details({ match }) {
+  const [character, setChar] = useState([]);
+
+  useEffect(() => {
+    const { id } = match.params;
+
+    getCharacter(id)
+      .then(chars => setChar(chars));
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    character.length === 0 ? <h1>Loading</h1>
+      : <CharacterDetails character={character}/>  
   );
 }
+
