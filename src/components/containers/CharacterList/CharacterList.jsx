e7598af7c57d1d8166f6357dde73bdecfc7eec5d
sformichella <sformichella@gmail.com>
2021-02-08 16:03:40 -0800

    CharacterList

--- src/components/containers/CharacterList/CharacterList.jsx
+++ src/components/containers/CharacterList/CharacterList.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getCharacters } from '../../../services/rickAndMorty';
+import List from '../../presentation/List/List';
 
 export default function CharacterList() {
+  const [characters, setChars] = useState([]);
+
+  useEffect(() => {
+    getCharacters()
+      .then(chars => setChars(chars));
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    characters.length === 0 ? <h1>Loading</h1>
+      : <List characters={characters}/>  
   );
 }
